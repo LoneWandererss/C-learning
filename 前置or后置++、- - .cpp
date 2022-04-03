d5e90e1d73110bc3705036971e93ef11de976691
LoneWandererss <100787103+LoneWandererss@users.noreply.github.com>
2022-04-03 19:02:06 +0800

    Add files via upload

---
+++ 前置or后置++、- - .cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{
+	int a = 10;
+	int b;
+	b = ++a;                //先++，再使用 
+	printf("a = %d\n",a);
+	printf("b = %d\n",b);
+	int c = 10;
+	int d;
+	d = c++;                //先使用，再++ 
+	printf("c = %d\n",c);
+	printf("d = %d\n",d);
+	return 0;
+}
