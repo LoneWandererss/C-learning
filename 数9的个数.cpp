4201db1b75c9f851b4cb0f5365dd61f6ce87a498
LoneWandererss <100787103+LoneWandererss@users.noreply.github.com>
2022-04-13 22:24:53 +0800

    Add files via upload

---
+++ 数9的个数.cpp
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main(void)
+{
+	int count = 0;
+	int i = 0;
+	for(i=1;i<=100;i++)
+	{
+		if(i%10 == 9)
+		{
+			count++;  //个位 
+		}
+		if(i/10 == 9)
+		{
+			count++;  //十位 
+		}
+	}
+	printf("count = %d\n",count);
+	return 0;
+}
